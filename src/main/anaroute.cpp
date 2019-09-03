5f1fe6fcaa887ed2d9f5b41ee4ea9510d68caaf8
Hao Chen <haoc@utexas.edu>
2019-09-03 00:18:28 -0500

    fix

--- src/main/anaroute.cpp
+++ src/main/anaroute.cpp
@@ -23,11 +23,11 @@ void Anaroute::parseArgs(int argc, char** argv) {
   _args.add<string>("tech_file", '\0', "technology layer file");
   _args.add<string>("design_type", '\0', "design type");
   _args.add<string>("design_file", '\0', "design file");
-  _args.add<string>("tech_file", '\0', "design technology file");
   _args.add<string>("placement_layout", '\0', "placement layout file");
   _args.add<string>("iopin", '\0', "IO-Pin file");
   _args.add<string>("symnet", '\0', "symmetric nets file");
   _args.add<string>("out", 'o', "output file");
+  _args.parse_check(argc, argv);
 }
 
 
