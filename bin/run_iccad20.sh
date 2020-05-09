d7c97d0a4c94909afe4ca613ac1351572b822756
Hao Chen <haoc@utexas.edu>
2020-05-09 02:58:43 -0500

    good

--- bin/run_iccad20.sh
+++ bin/run_iccad20.sh
@@ -26,23 +26,14 @@ GDB=""
           #--spec $BENCH_DIR/Core_test_flow/Core_test_flow.gr.spec \
           #--out $OUT_DIR/gds/Core_test_flow.route.gds
 
-# OTA_XT_MAGICAL_2 on grid power
-#$GDB ./anaroute --tech_lef $HAO_WORKSPACE/packages/tech/tsmc40_hvh_tapeout.lef \
-          #--tech_file $HAO_WORKSPACE/packages/tech/tsmc40_techfile \
-          #--design_file $BENCH_DIR/OTA_XT/OTA_XT_MAGICAL.gr \
-          #--placement_layout $BENCH_DIR/OTA_XT/OTA_XT_MAGICAL.place.gds \
-          #--iopin $BENCH_DIR/OTA_XT/OTA_XT_MAGICAL.iopin \
-          #--spec $BENCH_DIR/OTA_XT/OTA_XT_MAGICAL.gr.spec \
-          #--out $OUT_DIR/gds/OTA_XT_MAGICAL.route.gds
-
-# OTA_XT_MAGICAL_2 on grid power
-#$GDB ./anaroute --tech_lef $HAO_WORKSPACE/packages/tech/tsmc40_hvh_tapeout.lef \
-          #--tech_file $HAO_WORKSPACE/packages/tech/tsmc40_techfile \
-          #--design_file $BENCH_DIR/OTA_XT_2/OTA_XT_MAGICAL_2.gr \
-          #--placement_layout $BENCH_DIR/OTA_XT_2/OTA_XT_MAGICAL_2.place.gds \
-          #--iopin $BENCH_DIR/OTA_XT_2/OTA_XT_MAGICAL_2.iopin \
-          #--spec $BENCH_DIR/OTA_XT_2/OTA_XT_MAGICAL_2.gr.spec \
-          #--out $OUT_DIR/gds/OTA_XT_MAGICAL.route.gds
+# OTA_XT_MAGICAL on grid power
+$GDB ./anaroute --tech_lef $HAO_WORKSPACE/packages/tech/tsmc40_hvh_tapeout.lef \
+          --tech_file $HAO_WORKSPACE/packages/tech/tsmc40_techfile \
+          --design_file $BENCH_DIR/OTA_XT/OTA_XT_MAGICAL.gr \
+          --placement_layout $BENCH_DIR/OTA_XT/OTA_XT_MAGICAL.place.gds \
+          --iopin $BENCH_DIR/OTA_XT/OTA_XT_MAGICAL.iopin \
+          --spec $BENCH_DIR/OTA_XT/OTA_XT_MAGICAL.gr.spec \
+          --out $OUT_DIR/gds/OTA_XT_MAGICAL.route.gds
 
 # Telescopic on grid power
 #$GDB ./anaroute --tech_lef $HAO_WORKSPACE/packages/tech/tsmc40_hvh_tapeout.lef \
@@ -72,20 +63,11 @@ GDB=""
           #--iopin $BENCH_DIR/Core_test_flow/Core_test_flow.iopin \
           #--out $OUT_DIR/ARouter/Core_test_flow.route.gds
 
-$GDB ./ARouter --designType ispd08pin \
-          --techLef $HAO_WORKSPACE/packages/tech/tsmc40_hvh_tapeout.lef \
-          --techfile $HAO_WORKSPACE/packages/tech/tsmc40_techfile \
-          --designFile $BENCH_DIR/OTA_XT/OTA_XT_MAGICAL.gr.ispd08 \
-          --placementLayout $BENCH_DIR/OTA_XT/OTA_XT_MAGICAL.place.gds \
-          --symnet $BENCH_DIR/OTA_XT/OTA_XT_MAGICAL.symnet \
-          --iopin $BENCH_DIR/OTA_XT/OTA_XT_MAGICAL.iopin \
-          --out $OUT_DIR/ARouter/OTA_XT_MAGICAL.route.gds
-
 #$GDB ./ARouter --designType ispd08pin \
           #--techLef $HAO_WORKSPACE/packages/tech/tsmc40_hvh_tapeout.lef \
           #--techfile $HAO_WORKSPACE/packages/tech/tsmc40_techfile \
-          #--designFile $BENCH_DIR/OTA_XT_2/OTA_XT_MAGICAL_2.gr.ispd08 \
-          #--placementLayout $BENCH_DIR/OTA_XT_2/OTA_XT_MAGICAL_2.place.gds \
-          #--symnet $BENCH_DIR/OTA_XT_2/OTA_XT_MAGICAL_2.symnet \
-          #--iopin $BENCH_DIR/OTA_XT_2/OTA_XT_MAGICAL_2.iopin \
-          #--out $OUT_DIR/ARouter/OTA_XT_MAGICAL_2.route.gds
+          #--designFile $BENCH_DIR/OTA_XT/OTA_XT_MAGICAL.gr.ispd08 \
+          #--placementLayout $BENCH_DIR/OTA_XT/OTA_XT_MAGICAL.place.gds \
+          #--symnet $BENCH_DIR/OTA_XT/OTA_XT_MAGICAL.symnet \
+          #--iopin $BENCH_DIR/OTA_XT/OTA_XT_MAGICAL.iopin \
+          #--out $OUT_DIR/ARouter/OTA_XT_MAGICAL.route.gds
